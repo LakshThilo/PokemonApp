256f1e65db3d24f31aa95052e1864ba32df6b7ec
Lakshitha <lakshthilo9094@gmail.com>
2022-06-15 20:48:53 +0100

    Adding hilt module for retrofit and adding application class

---
+++ app/src/main/java/com/lak/pokemonapp/di/AppModule.kt
@@ -0,0 +1,53 @@
+package com.lak.pokemonapp.di
+
+
+
+import com.lak.pokemonapp.model.remote.apiservice.PokeApi
+import com.lak.pokemonapp.utils.Constant.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokeApi(retrofit: Retrofit): PokeApi {
+        return retrofit.create(PokeApi::class.java)
+    }
+
+ }
